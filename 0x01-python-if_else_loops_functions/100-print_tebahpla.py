8b59a04bf98f8d93f05d928a4e900db39bfb074c
victormayowa <hardmanvicky01@gmail.com>
2023-06-06 14:04:08 -0700

    the rest of the task

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+for i in range(122, 96, -1):
+    print("{:c}".format(i), end="")
+    print("{:c}".format(i - 32), end="")
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    for c in str:
+        if ord(c) >= 97 and ord(c) <= 122:
+            uppercase_char = chr(ord(c) - 32)
+        else:
+            uppercase_char = c
+        print("{:s}".format(uppercase_char), end="")
+    print()
