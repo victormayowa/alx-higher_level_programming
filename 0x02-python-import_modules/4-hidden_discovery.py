5e355c31c4c6611e58c98543254df968e2658dc0
victormayowa <hardmanvicky01@gmail.com>
2023-06-08 16:41:14 -0700

    hidden love

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,21 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/python3.8
 
 if __name__ == "__main__":
-    import py_compile
-    py_compile.compile("hidden_4.py")
-    with open("hidden_4.pyc", "rb") as file:
-        magic = file.read(4)  # Read the magic number
-        timestamp = file.read(4)  # Read the timestamp
-        file.read(4)  # Ignore the size field               
-        names = []
-        while True:
-            byte = file.read(1)
-            if byte == b'':
-                break
-            size = int.from_bytes(byte, byteorder='little')
-            name = file.read(size).decode()  # Read the name
-            if not name.startswith("__"):
-                names.append(name)
-            names.sort()
-            for name in names:
-                print("{}".format(name))
+    import hidden_4
+    for e in dir(hidden_4_pyc):
+        if e[0] != "_":
+            print("{}".format(e))
