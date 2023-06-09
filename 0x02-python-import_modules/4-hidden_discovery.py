6eee2eba9fc44a8c0054ea3a6f533705ab169873
victormayowa <hardmanvicky01@gmail.com>
2023-06-08 17:02:15 -0700

    impot variable

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3.8
 
 if __name__ == "__main__":
+    import sys
     import hidden_4
-    for e in dir(hidden_4_pyc):
-        if e[0] != "_":
-            print("{}".format(e))
+    module_names = dir(hidden_4)
+    filtered_names = sorted(name for name in module_names if not name.startswith("__"))
+    for name in filtered_names:
+        for name in filtered_names:
+            print("{}".format(name))
+
