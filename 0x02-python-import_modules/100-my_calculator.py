0053aab5bba3eeaf3c0b651e1c23b37f4dacde73
victormayowa <hardmanvicky01@gmail.com>
2023-06-14 15:22:41 -0700

    final

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,20 +4,20 @@ if __name__ == '__main__':
     from calculator_1 import add, sub, mul, div
     argc = len(sys.argv)
     if argc != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        print('Usage: {} <a> <operator> <b>'.format(sys.argv[0]))
         sys.exit(1)
-        a = int(sys.argv[1])
-        operator = sys.argv[2]
-        b = int(sys.argv[3])
-        if operator == '+':
-            result = add(a, b)
-        elif operator == '-':
-            result = sub(a, b)
-        elif operator == '*':
-            result = mul(a, b)
-        elif operator == '/':
-            result = div(a, b)
-        else:
-            print("Unknown operator. Available operators: +, -, * and /")
-            sys.exit(1)
-        print("{} {} {} = {}".format(a, operator, b, result))
+    a = int(sys.argv[1])
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
+    if operator == '+':
+        result = add(a, b)
+    elif operator == '-':
+        result = sub(a, b)
+    elif operator == '*':
+        result = mul(a, b)
+    elif operator == '/':
+        result = div(a, b)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+    print("{} {} {} = {}".format(a, operator, b, result))
--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,1 +1,2 @@
 #!/usr/bin/python3
+sys.stdout.write("#pythoniscool\n")
