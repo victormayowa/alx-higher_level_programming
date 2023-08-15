ab0ccb8165b5d9b7c9116e30180896ec14d81183
victormayowa <hardmanvicky01@gmail.com>
2023-08-15 13:52:46 -0700

    describe

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,9 +1,3 @@
-/* 
-Prints the full description of the table first_table from the specified database
-Note: Replace 'your_database_name' with the actual database name.
-*/
-
-SELECT table_name, CREATE_TABLE
-FROM information_schema.tables
-WHERE table_schema = 'your_database_name' AND table_name = 'first_table';
-
+-- Prints the full description of the table first_table from the specified database
+-- Note: Replace 'your_database_name' with the actual database name.
+SHOW CREATE TABLE first_table
