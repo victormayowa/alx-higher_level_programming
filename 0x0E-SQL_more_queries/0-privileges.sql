c6010cce5fd9c8be2f4d16ad649e23834d330ac0
victormayowa <hardmanvicky01@gmail.com>
2023-08-16 13:36:00 -0700

    all

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,7 +1,7 @@
 -- Create user user_0d_2 if not exists
--- Grant privileges to user_0d_2 (modify privileges as needed)
--- List privileges for user_0d_1
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS
+	FOR 'user_0d_1'@'localhost';
 
 -- List privileges for user_0d_2
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SHOW GRANTS
+	FOR 'user_0d_2'@'localhost';
--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,8 @@
 -- Create user user_0d_1 if not exists
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
 
 -- Grant all privileges to user_0d_1
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL
+	ON *.*
+   	TO 'user_0d_1'@'localhost';
