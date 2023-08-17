8a57a8e351fe5b74f20f0acfb0c32bdb3cdbc176
victormayowa <hardmanvicky01@gmail.com>
2023-08-17 13:33:26 -0700

    aliases

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,9 +1,7 @@
--- Use the database hbtn_0d_tvshows
-USE hbtn_0d_tvshows;
-
 -- List all shows without a linked genre
-SELECT tv_shows.title, NULL AS genre_id
-FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-WHERE tv_show_genres.genre_id IS NULL
-ORDER BY tv_shows.title;
+SELECT shows.title, genres.genre_id
+FROM tv_shows AS shows
+LEFT JOIN tv_show_genres AS genres
+ON shows.id = genres.show_id
+WHERE genres.genre_id IS NULL
+ORDER BY shows.title, genres.genre_id;
