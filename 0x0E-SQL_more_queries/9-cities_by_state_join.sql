a39c1a5f91e2e4606b58114ea6d024593e3809c8
victormayowa <hardmanvicky01@gmail.com>
2023-08-17 12:56:46 -0700

    inner join

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,8 +1,5 @@
--- Use the database hbtn_0d_usa
-USE hbtn_0d_usa;
-
 -- List all cities with state names using a JOIN
 SELECT cities.id, cities.name, states.name
-FROM cities
-JOIN states ON cities.state_id = states.id
+	FROM cities INNER JOIN states
+		ON cities.state_id = states.id
 ORDER BY cities.id;
