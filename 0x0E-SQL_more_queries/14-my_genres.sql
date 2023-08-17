57184261a4b27c3a4956c40781b43ceb0c5dca8b
victormayowa <hardmanvicky01@gmail.com>
2023-08-17 13:51:00 -0700

    complex

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -2,8 +2,7 @@
 SELECT DISTINCT tv_genres.name
 FROM tv_genres
 INNER JOIN tv_show_genres AS genres
-ON tv_genres.id = genres.genre_id
 INNER JOIN tv_shows AS shows
-ON shows.id = genres.show.id
-WHERE shows.title = 'Dexter'
-ORDER BY tv_genres.name;
+ ON tv_genres.id = genres.genre_id AND shows.id = genres.show_id
+  WHERE shows.title = "Dexter"
+ ORDER BY tv_genres.name;
