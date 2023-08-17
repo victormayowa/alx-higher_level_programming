25f4f1de98db1858ee77d636b57c1bde478048cf
victormayowa <hardmanvicky01@gmail.com>
2023-08-17 13:01:39 -0700

    inner join and order by

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,8 +1,5 @@
--- Use the database hbtn_0d_tvshows
-USE hbtn_0d_tvshows;
-
 -- List all shows with at least one linked genre
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
