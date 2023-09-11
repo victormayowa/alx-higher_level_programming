3653b35733f8da69970459fdf1cae56096a16d1f
victormayowa <hardmanvicky01@gmail.com>
2023-09-11 15:34:04 -0700

    javset

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const arg = process.argv[2];
-if (arg === undefined) {
+const args = process.argv;
+if (typeof args[2] === 'undefined') {
   console.log('No argument');
 } else {
-  console.log(arg);
+  console.log(args[2]);
 }
