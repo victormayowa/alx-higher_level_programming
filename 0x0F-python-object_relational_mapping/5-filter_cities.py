949984014e2712373e192f3bac48868b47fc3031
victormayowa <hardmanvicky01@gmail.com>
2023-09-18 04:15:44 -0700

    filter cities

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -31,8 +31,8 @@ def filter_cities():
     cursor.execute("""SELECT cities.name
                     FROM cities
                     JOIN states ON cities.state_id = states.id
-                    WHERE states.name=%s
-                    ORDER BY cities.id ASC""", (state_name,))
+                    WHERE states.name='{}'
+                    ORDER BY cities.id ASC""".format(state_name))
 
     # Fetch and print the results
     cities = cursor.fetchall()
