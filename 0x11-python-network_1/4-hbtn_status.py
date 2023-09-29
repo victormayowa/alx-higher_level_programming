d14525313128691baee02bd78adeb275d373293f
victormayowa <hardmanvicky01@gmail.com>
2023-09-29 10:41:53 -0700

    status of hbtn

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Fetches https://alx-intranet.hbtn.io/status"""
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    response = requests.get(url)
+    content = response.text
+
+    print("Body response:")
+    print(f"\t- type: {type(content)}")
+    print(f"\t- content: {content}")
