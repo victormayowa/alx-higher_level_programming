bd87e6deaf13b3b29994c74a1ad98fb8e66226f1
victormayowa <hardmanvicky01@gmail.com>
2023-09-29 10:17:28 -0700

    more on netork

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
 import urllib.request
-''' Define the URL'''
-url = "https://alx-intranet.hbtn.io/status"
-# Open the URL using urllib.request.urlopen() in a with statement
-with urllib.request.urlopen(url) as response:
-    '''Read the content of the response'''
-    content = response.read()
-    # Print information about the response
-    print("Body response:")
-    print(f"    - type: {type(content)}")  # Print the type of the content
-    print(f"    - content: {content}")  # Print the raw content
-    print(f"    - utf8 content: {content.decode('utf-8')}")
+
+
+def status_code():
+    ''' Define the URL'''
+    url = "https://alx-intranet.hbtn.io/status"
+    # Open the URL using urllib.request.urlopen() in a with statement
+    with urllib.request.urlopen(url) as response:
+        '''Read the content of the response'''
+        content = response.read()
+        # Print information about the response
+        print("Body response:")
+        print("    - type: {}".format(type(content)))  # Print type of content
+        print("    - content: {}".format(content))  # Print the raw content
+        print("    - utf8 content: {}".format(content.decode('utf-8')))
+
+
+if __name__ == "__main__":
+    status_code()
