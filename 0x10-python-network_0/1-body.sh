b42cbf8c5fcfdb3916f83c2638919ff34da75c2c
victormayowa <hardmanvicky01@gmail.com>
2023-09-29 07:20:48 -0700

    repair task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes a URL as input, sends a GET request, and displays the body of the response
-if ["$(curl -sL -o /dev/null -sw "%{http_code}" "$1")" -eq 200 ]; then curl -sL "$1"; fi
+if [ "$(curl -sL -o /dev/null -sw '%{http_code}' "$1")" -eq 200 ]; then curl -sL "$1"; fi
