82537b779c9203956ed031eb18a89dce795264cb
victormayowa <hardmanvicky01@gmail.com>
2023-10-24 06:47:04 -0700

    3-starwars_title.js

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const request = require('request');
+
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+request.get(url, (error, response, body) => {
+	if (error) {
+		console.error(error);
+	} else if (response.statusCode === 200) {
+		const movieData = JSON.parse(body);
+		console.log(movieData.title);
+	} else {
+		console.error('Error: ${response.statusCode}');
+	}
+});
